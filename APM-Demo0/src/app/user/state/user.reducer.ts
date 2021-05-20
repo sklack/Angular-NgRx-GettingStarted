c68c5a5a16f96c850181c7e12bcc4125e05db5de
jenan <jenan.hong@sunbirddcim.com>
2021-05-20 14:58:46 +0800

    Build state action reducer select cycle in products users.

---
+++ APM-Demo0/src/app/user/state/user.reducer.ts
@@ -0,0 +1,11 @@
+import { createAction, createReducer, on } from "@ngrx/store";
+
+export const userReducer = createReducer(
+  { maskUserName: true },
+  on(createAction('[User] Mask User Name'), state => {
+    return {
+      ...state,
+      maskUserName: !state.maskUserName
+    }
+  })
+)
